3c80d384216f0fa9d2f6d59da7c2fd61905d57e8
Benoit Donneaux <benoit.donneaux@digital-me.nl>
2017-12-09 13:45:00 +0100

    Use config rather than options
    Signed-off-by: Benoit Donneaux <benoit.donneaux@digital-me.nl>

--- vars/stageCompile.groovy
+++ vars/stageCompile.groovy
@@ -1,11 +1,11 @@
 #!/usr/bin/env groovy
 
 def call() {
-	if (options.compile ) {
+	if (config.compile ) {
 		stage('Compilation') {
 			echo 'Compilation goes here'
 		}
 	} else {
-		echo "Compilation will be skipped (option.compile = ${options.compile})"
+		echo "Compilation will be skipped (config.compile = ${config.compile})"
 	}
 }
