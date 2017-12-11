0fb59138893502db379040c7d064d1c581e352a6
Benoit Donneaux <benoit.donneaux@digital-me.nl>
2017-12-11 20:09:18 +0100

    Fix wrong map reference
    Signed-off-by: Benoit Donneaux <benoit.donneaux@digital-me.nl>

--- vars/stCompile.groovy
+++ vars/stCompile.groovy
@@ -3,8 +3,8 @@
 def call(config, tasks = []) {
 	if (config.st.compile) {
 		stage('Compile') {
-			if (option.verbose) echo "Compilation for ${config.name} begins here"
-			if (option.verbose) echo "Compilation for ${config.name} ends here"
+			if (config.verbose) echo "Compilation for ${config.name} begins here"
+			if (config.verbose) echo "Compilation for ${config.name} ends here"
 		}
 	} else {
 		echo "Compilation will be skipped (config.st.compile = ${config.st.compile})"
