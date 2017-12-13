b32fb0c872dee972d67a9b0917f5c51007803ba7
Benoit Donneaux <benoit.donneaux@digital-me.nl>
2017-12-14 00:33:26 +0100

    See if we can avoid using legacy helpers
    Signed-off-by: Benoit Donneaux <benoit.donneaux@digital-me.nl>

--- vars/libraryHelpers.groovy
+++
@@ -1,6 +0,0 @@
-def loadAndDump(name, file=null) {
-	file = file || name
-	def preparePackage = libraryResource name
-    writeFile file: file, text: preparePackage
-    sh "chmod 777 $file"
-}
--- vars/stValidate.groovy
+++ vars/stValidate.groovy
@@ -5,7 +5,24 @@ def call(config, tasks = []) {
 		stage('Validate') {
 			if (config.verbose) echo "Validation for ${config.name} begins here"
 			node(label: config.lb.docker) {
-				staticCodeValidation()
+				try {
+					echo 'Static code validation'
+					// Checkout the source
+					checkout scm
+					// Extract Dockerfile from shared lib to 'ci' folder 
+					writeFile(
+						file: 'ci/Dockerfile',
+						text: libraryResource("code-validation.dockerfile")
+					)
+					// Build docker image from 'ci' folder and use flake to validate
+					docker.build('code-validation', 'ci').inside {
+						sh "python3 -m flake8"
+					}
+				}
+				finally {
+					echo 'Static code validation: Cleanup'
+					step([$class: 'WsCleanup'])
+				}
 			}
 			if (config.verbose) echo "Validation for ${config.name} ends here"
 		}
