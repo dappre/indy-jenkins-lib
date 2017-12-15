9e741cad3a2e23c334472913b1348eecc737df8d
Benoit Donneaux <benoit.donneaux@digital-me.nl>
2017-12-15 12:19:44 +0100

    Validate in parallel
    Signed-off-by: Benoit Donneaux <benoit.donneaux@digital-me.nl>

--- vars/stValidate.groovy
+++ vars/stValidate.groovy
@@ -2,32 +2,44 @@
 
 package nl.digitalme.indy
 
+def stLinux(String dist) {
+	// Extract Dockerfile from shared lib to 'ci' folder
+	writeFile(
+			file: 'ci/Dockerfile',
+			text: libraryResource('dist/' + dist + '/validate.dockerfile')
+			)
+	// Build docker image from 'ci' folder and use flake to validate
+	docker.build(config.name + '-validate-' + dist + ':' + config.branch, 'ci').inside { sh "python3.5 -m flake8" }
+}
+
 def call(config, tasks = []) {
 	if (config.st.validate) {
 		stage('Validate') {
 			if (config.verbose) echo "Validation for ${config.name} begins here"
-			node(label: config.lb.docker) {
-				try {
-					echo 'Static code validation'
-					// Checkout the source
-					checkout scm
-					config.dists.each { dist ->
-						// Extract Dockerfile from shared lib to 'ci' folder
-						writeFile(
-								file: 'ci/Dockerfile',
-								text: libraryResource('dist/' + dist + '/validate.dockerfile')
-								)
-						// Build docker image from 'ci' folder and use flake to validate
-						docker.build(config.name + '-validate-' + dist + ':' + config.branch, 'ci').inside { sh "python3.5 -m flake8" }
+			echo 'Static code validation'
+			// Checkout the source
+			checkout scm
+			parallel(
+			config.dists[0]: {
+				node(label: config.lb.docker) {
+					try {
+						stLinux(config.dists[0])
 					}
 				}
-				finally {
-					echo 'Static code validation: Cleanup'
-					step([$class: 'WsCleanup'])
+			},
+			config.dists[1]: {
+				node(label: config.lb.docker) {
+					try {
+						stLinux(config.dists[1])
+					}
 				}
-			}
-			if (config.verbose) echo "Validation for ${config.name} ends here"
+			})
+			//				finally {
+			echo 'Static code validation: Cleanup'
+			step([$class: 'WsCleanup'])
+			//				}
 		}
+		if (config.verbose) echo "Validation for ${config.name} ends here"
 	} else {
 		echo "Validation will be skipped (config.st.validate = ${config.st.validate})"
 	}
