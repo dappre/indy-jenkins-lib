1a7489287bd5bdbae6479750279563b8db86b193
Benoit Donneaux <benoit.donneaux@digital-me.nl>
2017-12-15 13:36:30 +0100

    Fix Map definition
    Signed-off-by: Benoit Donneaux <benoit.donneaux@digital-me.nl>

--- vars/stValidate.groovy
+++ vars/stValidate.groovy
@@ -23,7 +23,7 @@ def valDocker(config, String dist) {
 
 // Generates a Map of node to execute validation steps per distribution
 Map distNode(ArrayList dists) {
-	Map mDists = null
+	Map mDists = []
 	
 	dists.each { dist ->
 		if (dist =~ /^(win|mac)/) {
